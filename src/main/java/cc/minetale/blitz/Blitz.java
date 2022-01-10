eb41694c75d8915c3b3ebc3eae48120357715ad3
oHate <ohateign@gmail.com>
2022-01-10 01:59:28 -0600

    File Changes

--- src/main/java/cc/minetale/blitz/Blitz.java
+++ src/main/java/cc/minetale/blitz/Blitz.java
@@ -3,26 +3,13 @@ package cc.minetale.blitz;
 import cc.minetale.blitz.listeners.pigeon.PlayerListener;
 import cc.minetale.blitz.listeners.velocity.PlayerEvents;
 import cc.minetale.commonlib.CommonLib;
-import cc.minetale.commonlib.api.Pterodactyl;
-import cc.minetale.commonlib.server.ServerType;
 import cc.minetale.pigeon.Pigeon;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.proxy.ProxyServer;
-import com.velocitypowered.api.proxy.server.RegisteredServer;
-import com.velocitypowered.api.proxy.server.ServerInfo;
 import lombok.Getter;
 import org.slf4j.Logger;
-
-import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @Getter
 public class Blitz {
@@ -30,7 +17,6 @@ public class Blitz {
     @Getter private static Blitz blitz;
     private final ProxyServer proxyServer;
     private final Logger logger;
-    private final Map<ServerType, Set<RegisteredServer>> serverMap;
 
     @Inject
     public Blitz(ProxyServer proxyServer, Logger logger) {
@@ -38,11 +24,6 @@ public class Blitz {
 
         this.proxyServer = proxyServer;
         this.logger = logger;
-        this.serverMap = new HashMap<>();
-
-        for(var type : ServerType.values()) {
-            this.serverMap.put(type, new HashSet<>());
-        }
     }
 
     @Subscribe
@@ -50,27 +31,7 @@ public class Blitz {
         long start = System.currentTimeMillis();
 
         CommonLib.init();
-
         Pigeon.getPigeon().getListenersRegistry().registerListener(new PlayerListener());
-
-        try {
-            var servers = Pterodactyl.getServers().get(5, TimeUnit.SECONDS);
-
-            for(var server : servers) {
-                var allocation = Pterodactyl.getPteroApplication().retrieveAllocationById(server.getDefaultAllocationId()).execute();
-                var type = ServerType.valueOf(server.getExternalId().split(":")[1]);
-                var registeredServer = this.proxyServer.registerServer(new ServerInfo(server.getName(), new InetSocketAddress(allocation.getIP(), allocation.getPortInt())));
-
-                System.out.println("Successfully Registered: " + server.getName() + " of type " + type.name());
-                this.serverMap.get(type).add(registeredServer);
-            }
-
-            for(var server : this.serverMap.get(ServerType.HUB)) {
-                this.proxyServer.getConfiguration().getAttemptConnectionOrder().add(server.getServerInfo().getName());
-            }
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            e.printStackTrace();
-        }
 
         this.proxyServer.getEventManager().register(this, new PlayerEvents());
         System.out.println("Done (" + (System.currentTimeMillis() - start) + "ms)! Blitz has successfully been initialized!");
