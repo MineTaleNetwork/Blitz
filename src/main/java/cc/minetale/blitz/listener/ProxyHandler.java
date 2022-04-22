625716675cfd7341053ec9785bbbc0b316fe40d1
Mint <ohateign@gmail.com>
2022-04-22 16:09:24 -0500

    Added PunishmentAddPayload to handler

--- src/main/java/cc/minetale/blitz/listener/ProxyHandler.java
+++ src/main/java/cc/minetale/blitz/listener/ProxyHandler.java
@@ -4,7 +4,14 @@ import cc.minetale.blitz.Blitz;
 import cc.minetale.blitz.Staff;
 import cc.minetale.sodium.lang.Language;
 import cc.minetale.sodium.profile.Profile;
+import cc.minetale.sodium.profile.grant.Grant;
+import cc.minetale.sodium.profile.punishment.Punishment;
 import cc.minetale.sodium.util.Message;
+import cc.minetale.sodium.util.TimeUtil;
+import com.velocitypowered.api.proxy.Player;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class ProxyHandler {
 
@@ -49,6 +56,58 @@ public class ProxyHandler {
             var oPlayer = proxy.getPlayer(friend);
 
             oPlayer.ifPresent(player -> player.sendMessage(friendMessage));
+        }
+    }
+
+    public static void addGrant(Player player, Grant grant) {
+        var rank = grant.getRank();
+
+        player.sendMessage(Message.chatSeparator());
+        player.sendMessage(Message.notification("Grant",
+                Component.text().append(
+                        Component.text("A ", NamedTextColor.GRAY),
+                        Component.text(rank.getName(), rank.getColor()),
+                        Component.text(" grant has been applied to you " +
+                                (grant.getDuration() == Integer.MAX_VALUE ? "permanently" :
+                                        "for " + TimeUtil.millisToRoundedTime(grant.getDuration())) + ".", NamedTextColor.GRAY)
+                ).build()));
+        player.sendMessage(Message.chatSeparator());
+    }
+
+    public static void removeGrant(Player player, Grant grant) {
+        var rank = grant.getRank();
+
+        player.sendMessage(Message.chatSeparator());
+        player.sendMessage(Message.notification("Grant",
+                Component.text().append(
+                        Component.text("Your ", NamedTextColor.GRAY),
+                        Component.text(rank.getName(), rank.getColor()),
+                        Component.text(" grant has been removed.", NamedTextColor.GRAY)
+                ).build()));
+        player.sendMessage(Message.chatSeparator());
+    }
+
+    public static void expireGrant(Player player, Grant grant) {
+        var rank = grant.getRank();
+
+        player.sendMessage(Message.chatSeparator());
+        player.sendMessage(Message.notification("Grant",
+                Component.text().append(
+                        Component.text("Your ", NamedTextColor.GRAY),
+                        Component.text(rank.getName(), rank.getColor()),
+                        Component.text(" grant has expired.", NamedTextColor.GRAY)
+                ).build()));
+        player.sendMessage(Message.chatSeparator());
+    }
+
+    public static void addPunishment(Player player, Punishment punishment) {
+        switch (punishment.getType()) {
+            case BAN -> player.disconnect(Component.join(JoinConfiguration.newlines(), punishment.getPunishmentMessage()));
+            case MUTE -> {
+                for(var component : punishment.getPunishmentMessage()) {
+                    player.sendMessage(component);
+                }
+            }
         }
     }
 
--- src/main/java/cc/minetale/blitz/listener/pigeon/GenericListener.java
+++ src/main/java/cc/minetale/blitz/listener/pigeon/GenericListener.java
@@ -1,8 +1,11 @@
 package cc.minetale.blitz.listener.pigeon;
 
+import cc.minetale.blitz.Blitz;
 import cc.minetale.blitz.listener.ProxyHandler;
 import cc.minetale.postman.payload.PayloadHandler;
 import cc.minetale.postman.payload.PayloadListener;
+import cc.minetale.sodium.payloads.GrantPayload;
+import cc.minetale.sodium.payloads.PunishmentAddPayload;
 import cc.minetale.sodium.payloads.proxy.ProxyPlayerConnectPayload;
 import cc.minetale.sodium.payloads.proxy.ProxyPlayerDisconnectPayload;
 import cc.minetale.sodium.payloads.proxy.ProxyPlayerSwitchPayload;
@@ -22,6 +25,28 @@ public class GenericListener implements PayloadListener {
     @PayloadHandler
     public void onProxyPlayerDisconnect(ProxyPlayerDisconnectPayload payload) {
         ProxyHandler.proxyPlayerDisconnect(payload.getProfile(), payload.getServer());
+    }
+
+    @PayloadHandler
+    public void onGrant(GrantPayload payload) {
+        var oPlayer = Blitz.getBlitz().getServer().getPlayer(payload.getPlayer());
+
+        oPlayer.ifPresent(player -> {
+            var grant = payload.getGrant();
+
+            switch (payload.getAction()) {
+                case ADD -> ProxyHandler.addGrant(player, grant);
+                case EXPIRE -> ProxyHandler.expireGrant(player, grant);
+                case REMOVE -> ProxyHandler.removeGrant(player, grant);
+            }
+        });
+    }
+
+    @PayloadHandler
+    public void onProxyPlayerDisconnect(PunishmentAddPayload payload) {
+        var oPlayer = Blitz.getBlitz().getServer().getPlayer(payload.getPlayer());
+
+        oPlayer.ifPresent(player -> ProxyHandler.addPunishment(player, payload.getPunishment()));
     }
 
 }
